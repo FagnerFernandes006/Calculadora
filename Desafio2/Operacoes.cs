65fb070343b9ff7fcfc452b37f2a55c95c4c490b
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2022-05-24 22:54:09 -0300

    Calculadora com dois valores

---
+++ Desafio2/Operacoes.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+
+namespace Desafio2
+{
+    public class Operacoes
+    {
+        public double valor1 { get; set; }
+        public double valor2 { get; set; }
+
+        public void Adicao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação Adição");
+            Console.WriteLine("-----------------");
+            Console.Write("Digite Dois números: ");
+            string[] vect = Console.ReadLine().Split(' ');
+            double n1 = double.Parse(vect[0], CultureInfo.InvariantCulture);
+            double n2 = double.Parse(vect[1], CultureInfo.InvariantCulture);
+            double resultado = n1 + n2;
+            Console.WriteLine("Resultado: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        public void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação Subtração");
+            Console.WriteLine("-----------------");
+            Console.Write("Digite Dois números: ");
+            string[] vect = Console.ReadLine().Split(' ');
+            double n1 = double.Parse(vect[0], CultureInfo.InvariantCulture);
+            double n2 = double.Parse(vect[1], CultureInfo.InvariantCulture);
+            double resultado = n1 - n2;
+            Console.WriteLine("Resultado: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        public void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação Multiplicação");
+            Console.WriteLine("-----------------");
+            Console.Write("Digite Dois números: ");
+            string[] vect = Console.ReadLine().Split(' ');
+            double n1 = double.Parse(vect[0], CultureInfo.InvariantCulture);
+            double n2 = double.Parse(vect[1], CultureInfo.InvariantCulture);
+            double resultado = n1 * n2;
+            Console.WriteLine("Resultado: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        public void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação Divisão");
+            Console.WriteLine("-----------------");
+            Console.Write("Digite Dois números: ");
+            string[] vect = Console.ReadLine().Split(' ');
+            double n1 = double.Parse(vect[0], CultureInfo.InvariantCulture);
+            double n2 = double.Parse(vect[1], CultureInfo.InvariantCulture);
+            double resultado = n1 / n2;
+            Console.WriteLine("Resultado: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        public void Raiz()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação Raiz Quadrada");
+            Console.WriteLine("-----------------");
+            Console.Write("Digite um número: ");
+            double n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double resultado = Math.Sqrt(n);
+            Console.WriteLine("Resultado: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        public void Potenciacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação Potenciação");
+            Console.WriteLine("-----------------");
+            Console.Write("Digite dois números: ");
+            string[] vect = Console.ReadLine().Split(' ');
+            double n1 = double.Parse(vect[0], CultureInfo.InvariantCulture);
+            double n2 = double.Parse(vect[1], CultureInfo.InvariantCulture);
+            double resultado = Math.Pow(n1, n2);
+            Console.WriteLine("Resultado: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Desafio2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacoes operacoes = new Operacoes();
+
+            Console.WriteLine("Qual operação você deseja fazer? ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Raiz Quadrada");
+            Console.WriteLine("6 - Potenciação");
+            Console.WriteLine("-------------------");
+            Console.Write("Operação: ");
+            int op = int.Parse(Console.ReadLine());
+
+            if (op == 1)
+            {
+                operacoes.Adicao();
+            }
+            else if (op == 2)
+            {
+                operacoes.Subtracao();
+            }
+            else if (op == 3)
+            {
+                operacoes.Multiplicacao();
+            }
+            else if (op == 4)
+            {
+                operacoes.Divisao();
+            }
+            else if (op == 5)
+            {
+                operacoes.Raiz();
+            }
+            else if (op == 6)
+            {
+                operacoes.Potenciacao();
+            }
+            else Console.WriteLine("Programa encerrado");
+        }
+    }
+}
